ddde2a4f57fc7e280839d03286533631cfc4fb05
YesVladess <vlad.kononenko95@gmail.com>
2021-04-20 01:16:16 +0300

    2 полезных экстеншена

--- workplaces/Presentation/Common/Extensions/String.swift
+++ workplaces/Presentation/Common/Extensions/String.swift
@@ -5,4 +5,20 @@
 //  Created by YesVladess on 20.04.2021.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+
+    private func attributes(font: UIFont, color: UIColor?) -> [NSAttributedString.Key: Any] {
+        return [
+            .font: font,
+            .foregroundColor: color ?? .black
+        ]
+    }
+
+    func addAttributes(font: UIFont, color: UIColor?) -> NSAttributedString {
+        return NSAttributedString(string: self,
+                                  attributes: attributes(font: font, color: color))
+    }
+
+}
--- workplaces/Presentation/Common/Extensions/UIColor.swift
+++ workplaces/Presentation/Common/Extensions/UIColor.swift
@@ -5,4 +5,31 @@
 //  Created by YesVladess on 20.04.2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+
+    public static var orange: UIColor {
+        UIColor(named: "orange") ?? UIColor.systemOrange
+    }
+
+    public static var grey: UIColor {
+        UIColor(named: "grey") ?? UIColor.gray
+    }
+
+    public static var lightGreyBlue: UIColor {
+        UIColor(named: "light_grey_blue") ?? UIColor.gray
+    }
+
+    public static var lightGrey: UIColor {
+        UIColor(named: "light_grey") ?? UIColor.lightGray
+    }
+
+    public static var middleGrey: UIColor {
+        UIColor(named: "middle_grey") ?? UIColor.gray
+    }
+
+    public static var darkGrey: UIColor {
+        UIColor(named: "dark_grey") ?? UIColor.darkGray
+    }
+}
