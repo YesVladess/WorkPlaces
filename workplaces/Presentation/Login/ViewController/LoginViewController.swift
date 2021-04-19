e8f3a2cf4df00c0e052afac0b7726c256e29ddc4
YesVladess <vlad.kononenko95@gmail.com>
2021-04-20 02:22:50 +0300

    Добавил вью лоадера + экстенш для включения

--- workplaces/Presentation/Login/ViewController/LoginViewController.swift
+++ workplaces/Presentation/Login/ViewController/LoginViewController.swift
@@ -15,4 +15,15 @@ class LoginViewController: UIViewController {
         add(child)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        view.showLoader()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        sleep(2)
+        view.removeLoader()
+    }
+
 }
