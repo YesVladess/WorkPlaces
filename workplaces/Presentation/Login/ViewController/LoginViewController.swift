3e2fe6946fd2bc5466a5497f308fa30a82a9f17f
YesVladess <vlad.kononenko95@gmail.com>
2021-04-19 18:39:02 +0300

    Перераскидал файлы

--- workplaces/Presentation/Login/ViewController/LoginViewController.swift
+++ workplaces/Presentation/Login/ViewController/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  workplaces
 //
 //  Created by YesVladess on 18.04.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
