d5281d26db430efbc9408f37f7f92cf64ee910f1
YesVladess <vlad.kononenko95@gmail.com>
2021-04-20 11:40:02 +0300

    Довыделил Story

---
+++ workplaces/Presentation/NewPost/NewPostViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewPostViewController.swift
+//  workplaces
+//
+//  Created by YesVladess on 20.04.2021.
+//
+
+import UIKit
+
+class NewPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ workplaces/Presentation/Profile/ViewController/FavoriteFeedViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteFeedViewController.swift
+//  workplaces
+//
+//  Created by YesVladess on 20.04.2021.
+//
+
+import Foundation
